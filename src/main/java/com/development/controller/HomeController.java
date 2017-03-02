fa8e0bdccb2e6b2d280139646b99608404bff1f1
Basavarajappa <insbasavar@ad.harman.com>
2017-03-03 01:35:04 +0530

    checking

--- src/main/java/com/development/controller/HomeController.java
+++ src/main/java/com/development/controller/HomeController.java
@@ -1,18 +1,33 @@
 package com.development.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.development.dao.UserDAO;
+import com.development.model.User;
+
 @Controller
 public class HomeController {
 
+	@Autowired
+    private UserDAO userDao;
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
 	}
+	
+	@RequestMapping(value="/home")
+    public ModelAndView home() {
+        List<User> listUsers = userDao.list();
+        ModelAndView model = new ModelAndView("home");
+        model.addObject("userList", listUsers);
+        return model;
+    }
 }
